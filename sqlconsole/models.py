132cbe54aac01edd07696b3c65a74ae8268887c0
ebsuku <stumenz.complex@gmail.com>
2022-10-09 23:01:49 +0200

    add user field

--- sqlconsole/models.py
+++ sqlconsole/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 from enum import IntEnum
 
 
@@ -11,6 +12,7 @@ class QueryLog(models.Model):
     query = models.TextField()
     created = models.DateTimeField(auto_now=True)
     state = models.SmallIntegerField()
+    created_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True)
 
     class Meta:
         permissions = [('can_execute_query', 'Can Execute Query')]
