fb0b7780143f9747d07c600259e88d2b93efe9b8
knightebsuku <midnight.complex@protonmail.com>
2024-06-07 14:16:55 +0200

    Block queries that can modify db

--- sqlconsole/forms.py
+++ sqlconsole/forms.py
@@ -14,11 +14,12 @@ class ConsoleForm(forms.ModelForm):
     def clean_query(self):
         sql = self.cleaned_data.get('query')
         sql = sql.lower()
-        if 'update' in sql and 'sqlconsole_querylog' in sql:
-            raise ValidationError('Invalid SQL')
-
-        if 'delete' in sql and 'sqlconsole_querylog' in sql:
-            raise ValidationError('Invalid SQL')
+        if 'update' in sql or 'delete' in sql or 'insert' in sql:
+            raise ValidationError('Write queries not allowed')
+        if 'sqlconsole_querylog' in sql:
+            raise ValidationError('Invalid sql table')
+        if ';' in sql:
+            raise ValidationError('Multiple sql queries not allowed.')
         return sql
 
 
