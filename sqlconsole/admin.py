b6fc6047d2dcd96a8cf6cffd789621180afad1c7
knightebsuku <midnight.complex@protonmail.com>
2024-06-10 15:44:12 +0200

    Allow for console results to be downloaded

--- sqlconsole/admin.py
+++ sqlconsole/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
 
-from .views import ConsoleView
-
-from .models import QueryLog
+from sqlconsole.views import ConsoleView
+from sqlconsole.models import QueryLog
+from sqlconsole.download import create_csv
 
 
 @admin.register(QueryLog)
@@ -13,9 +14,16 @@ class QueryLogAdmin(admin.ModelAdmin):
     def get_urls(self):
         urls = super().get_urls()
         console_url = [
-            path('query/', self.admin_site.admin_view(ConsoleView.as_view()), name='console')
+            path('query/', self.admin_site.admin_view(ConsoleView.as_view()), name='console'),
+            path('download/<int:query_id>', self.admin_site.admin_view(self.download_results),
+                 name='download-console-csv')
         ]
         return console_url + urls
+
+    def download_results(self, request, query_id):
+        response = HttpResponse(content_type='text/csv', )
+        response['Content-Disposition'] = 'attachment; filename="download.csv"'
+        return create_csv(query_id, response)
 
     def has_add_permission(self, request):
         return False
